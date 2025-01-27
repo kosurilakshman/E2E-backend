0396b5164612c8640231d1ad032d78f037ed1aa1
kosurilakshman <Lakshman.kosuri@sanjittechsolutions.in>
2025-01-27 10:39:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ import { configDotenv } from "dotenv";
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(cors({
-  origin: ["https://e2e-frontend-woyl.onrender.com"],
+  origin: ["https://e2e-frontend-woyl.onrender.com","https://exquisite-narwhal-4cca21.netlify.app/user"],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Auth', 'X-Requested-With', 'Accept'],
  
